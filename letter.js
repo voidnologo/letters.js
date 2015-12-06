6e91ee79a32955bd48f3df2885a6d5d17359b8b6
voidnologo <void.no.logo@icloud.com>
2015-12-05 20:51:34 -0700

    more audio

--- letter.js
+++ letter.js
@@ -3,39 +3,40 @@ var currentLetter = '';
 function randomLetter(){
     var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var randomNumber = Math.floor(Math.random()*letters.length);
-    //return(letters.charAt(randomNumber));
     currentLetter = letters.charAt(randomNumber);
     document.getElementById('letter').innerHTML = currentLetter;
-    sayLetter(currentLetter)
+    playAudio(currentLetter)
 }
 
 function checkInput(){
     var userInput = document.getElementById('userInput').value.toUpperCase();
-    console.log("input" + userInput);
-    console.log("current" + currentLetter);
     if(userInput === currentLetter){
-        displayStatus('Ok', 'check-ok');
+        displayStatus('Ok', 'check-ok', 'Ok');
         setTimeout(randomLetter, 2000)
     }else{
-        displayStatus('Wrong', 'check-wrong');
+        displayStatus('Wrong', 'check-wrong', 'Uh_oh');
     }
 }
 
-function displayStatus(status, displayClass){
-    document.getElementById('status').innerHTML = status;
-    document.getElementById('status').className = displayClass;
-    document.getElementById('status').style.display = 'block';
+function displayStatus(status, displayClass, soundFile){
+    var statusEl = document.getElementById('status')
+    statusEl.innerHTML = status;
+    statusEl.className = displayClass;
+    statusEl.style.display = 'block';
+    playAudio(soundFile)
     setTimeout(clearStatus, 2000)
 }
 
 function clearStatus(){
-    document.getElementById('status').innerHTML = '';
-    document.getElementById('status').style.display = 'none';
-    document.getElementById('userInput').value = '';
-    document.getElementById('userInput').focus();
+    var statusEl = document.getElementById('status')
+    var inputEl = document.getElementById('userInput')
+    statusEl.innerHTML = '';
+    statusEl.style.display = 'none';
+    inputEl.value = '';
+    inputEl.focus();
 }
 
-function sayLetter(letter){
-    var sound = new Audio("audio/ogg/" + letter.toUpperCase() + ".ogg")
+function playAudio(soundFile){
+    var sound = new Audio("audio/mp3/" + soundFile.toUpperCase() + ".mp3")
     sound.play()
 }
